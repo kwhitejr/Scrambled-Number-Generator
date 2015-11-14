1d7f76c3c86b305404f357c6d2ed6a505012cc15
kwhitejr <kwhitejr.edu@gmail.com>
2015-11-14 09:55:36 -1000

    Npm test running

--- spec/generator-spec.js
+++ spec/generator-spec.js
@@ -1,4 +1,12 @@
-var should = chai.should();
-
 var chai = require('chai');
 var generator = require('../js/generator.js');
+
+var should = chai.should();
+var expect = chai.expect;
+
+describe('Scrambled Number Generator', function() {
+  var sandbox;
+  var newScrambledNumber = scrambledNumberGenerator;
+
+  it('should be a ');
+});

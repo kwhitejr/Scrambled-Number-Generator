58074f1cdab402e7891422b90546f8782feeacb1
kwhitejr <kwhitejr.edu@gmail.com>
2015-11-14 12:30:42 -1000

    Refactored generator, more checks, more tests

--- js/generator.js
+++ js/generator.js
@@ -1,31 +1,31 @@
 module.exports = function generator (amount) {
 
   var numArr = [];
-
-  if (typeof amount !== 'number') {
-    console.log('You must enter a number, lazybones.');
-  } else {
-    for (var i = 0; i < amount; i++) {
-      numArr[i] = i;
-    }
-    console.log(numArr);
-  /* The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle.
-  See https://github.com/coolaj86/knuth-shuffle */
-    var currentIndex = numArr.length, temporaryValue, randomIndex ;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = numArr[currentIndex];
-      numArr[currentIndex] = numArr[randomIndex];
-      numArr[randomIndex] = temporaryValue;
-    }
-    console.log(numArr);
+  amount = parseInt(amount, 10);
+  if (isNaN(amount) || amount <= 0) {
+    console.log('You must enter a postive integer, lazybones.');
+    return numArr;
   }
+  for (var i = 0; i < amount; i++) {
+    numArr[i] = i;
+  }
+  console.log(numArr);
+/* The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle.
+See https://github.com/coolaj86/knuth-shuffle */
+  var currentIndex = numArr.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = numArr[currentIndex];
+    numArr[currentIndex] = numArr[randomIndex];
+    numArr[randomIndex] = temporaryValue;
+  }
+  console.log(numArr);
   return numArr;
 };
--- spec/generator-spec.js
+++ spec/generator-spec.js
@@ -8,8 +8,37 @@ describe('Generator', function() {
   it('should be a function', function () {
     expect(generator).to.exist;
     expect(generator).to.be.a('function');
-    expect(generate()).to.deep.equal([], 'requires a parameter');
-    expect(generate(5)).to.not.deep.equal([], 'parameter must be a number');
-    expect(generator('doggies')).to.deep.equal([], 'as noted, you must enter a number');
   });
+
+
+  it('parameter must be a number', function () {
+    expect(generator()).to.deep.equal([], 'requires a parameter');
+    expect(generator(5)).to.not.deep.equal([], 'parameter must be a number');
+    expect(generator('doggies')).to.deep.equal([], 'as noted, you must enter a number');
+    expect(generator({doggies: 'bow-wow'})).to.deep.equal([], 'as noted, you must enter a number');
+    expect(generator(undefined)).to.deep.equal([], 'as noted, you must enter a number');
+    expect(generator(null)).to.deep.equal([], 'as noted, you must enter a number');
+  });
+
+  describe('the resulting array', function () {
+    it('array length must equal amount', function () {
+      expect(generator(5).length).to.equal(5, 'array length must equal amount');
+    });
+
+    it('generated arrays must be randomly ordered and unique', function () {
+      var arrayA = generator(10);
+      var arrayB = generator(10);
+      var arrayC = generator(10);
+
+      expect(arrayA).to.not.deep.equal(arrayB, 'must return a unique array upon each invocation');
+      expect(arrayB).to.not.deep.equal(arrayC, 'must return a unique array upon each invocation');
+      expect(arrayC).to.not.deep.equal(arrayA, 'must return a unique array upon each invocation');
+    });
+
+    it('largest array value must be less than amount', function () {
+      expect(generator(10)).to.not.contain(10, 'array value should contain all integers between 0 and "amount - 1" ');
+    });
+
+  });
+
 });
